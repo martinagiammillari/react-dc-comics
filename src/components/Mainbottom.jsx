1459000a05dc0ba861dd7dcc45b9f954b1f0d98c
martinagiammillari <martigiammi@gmail.com>
2025-11-25 20:29:19 +0100

    feat: style main bottom done

--- src/components/Mainbottom.jsx
+++ src/components/Mainbottom.jsx
@@ -1,3 +1,7 @@
+import style from "./Mainbottom.module.css"
+
+
+
 import comics from "../assets/img/buy-comics-digital-comics.png"
 import merch from "../assets/img/buy-comics-merchandise.png"
 import locator from "../assets/img/buy-comics-shop-locator.png"
@@ -6,7 +10,7 @@ import power from "../assets/img/buy-dc-power-visa.svg"
 
 function Mainbottom() {
     return (
-        <ul>
+        <ul className={`${style.myList} flex justify-center items-center`}>
             <li>
                 <img src={comics} alt="" /> DIGITAL COMICS
             </li>
