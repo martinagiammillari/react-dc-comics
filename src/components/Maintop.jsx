7567656c8c7e38d83cc0ec7fa0e4c852e72a1246
martinagiammillari <martigiammi@gmail.com>
2025-11-26 17:30:11 +0100

    feat: maintop done

--- src/components/Maintop.jsx
+++ src/components/Maintop.jsx
@@ -1,18 +1,29 @@
-
+import React from "react";
+import comics from "../components/comics";
 import style from "./Maintop.module.css";
 import comicsbg from "../assets/img/jumbotron.jpg";
 
 function Maintop() {
   return (
     <>
-      
-        <img className={style.comicsbg} src={comicsbg} alt="" />
-    
-      <div>
-        qua inserisco le card dinamicamente
+      <img className={style.comicsbg} src={comicsbg} alt="jumbotron" />
+
+      <div className="flex justify-center">
+        <div className={style.cardsGrid}>
+          {comics.map((comic) => (
+            <div className={style.card} key={comic.id}>
+              <img
+                className={style.cardImg}
+                src={comic.thumb}
+                alt={comic.title}
+              />
+              <h3 className={style.cardTitle}>{comic.series.toUpperCase()}</h3>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
 }
 
-export default Maintop; 
+export default Maintop;
